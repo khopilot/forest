13299eefe1ab5748f21aeeaa04c9145a84aedebd
khopilot <pienikdelrieu@gmail.com>
2025-02-04 15:57:07 +0700

    Major site update: - Add new pages (Merch, Crew, About) - Update project structure - Implement navigation system - Update site metadata - Add film project layouts

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
-import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
